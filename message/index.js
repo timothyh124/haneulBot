66dec1fd634bfd5701b06e95624c823fd69e2a6b
Timothy Han <timothyh124@gmail.com>
2018-05-30 23:19:59 -0400

    little work on index for testing

--- message/index.js
+++ message/index.js
@@ -6,13 +6,32 @@
  * @param {!Object} res Cloud Function response context.
  */
 
-const mysql = require('mysql');
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
 exports.message = (req, res) => {
 	/* required variables */
 	// parse request
   let user_key = decodeURIComponent(req.body.user_key); // user's key
   let type = decodeURIComponent(req.body.type); // message type
   let content = decodeURIComponent(req.body.content); // user's message
-	// initiate variables for responds
-    
+
+  /* firebase admin and db initialization */
+  admin.initializeApp(functions.config().firebase);
+  var db = admin.firestore();
+  let qry = db.collection('userInfo').where('userKey','==',user_key).get()
+      .then(snapshot => {
+        if(snapshot.length === 0) {
+          console.log("user NOT FOUND!")
+        } else if(snapshot.length === 1){
+          console.log("user FOUND!" + snapshot[0])
+        } else { // two different account under same user_key exist on database issue an warning
+          return Promise.reject('detected multiple of same user on database for { "userKey":' + user_key + '"}')
+        }
+      })
+      .catch(err=> {
+        console.log("Issue occured during user identification", err)
+      });
 };
+
+
